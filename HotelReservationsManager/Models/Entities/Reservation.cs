4a0f9141648e871e93c71709c16b17eacf36175e
Kamui <Kamuicho>
2024-04-03 21:39:44 +0300

    Wrote Reservation class.

--- HotelReservationsManager/Models/Entities/Reservation.cs
+++ HotelReservationsManager/Models/Entities/Reservation.cs
@@ -2,5 +2,14 @@
 {
     public class Reservation
     {
+        public Guid ID { get; set; }
+        public Room Room { get; set; }
+        public User Creator { get; set; }
+        public List<Client> Clients { get; set; }
+        public DateTime AccommodationDate { get; set; }
+        public DateTime LeaveDate { get; set; }
+        public bool Breakfast { get; set; }
+        public bool AllInclusive { get; set; }
+        public decimal Sum { get; set; }
     }
 }
