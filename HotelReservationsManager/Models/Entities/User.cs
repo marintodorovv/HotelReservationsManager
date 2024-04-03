08bd2f49b96e44bcdd7ba42b7c69500d146439be
Kamui <Kamuicho>
2024-04-03 21:39:27 +0300

    Wrote User class.

--- HotelReservationsManager/Models/Entities/User.cs
+++ HotelReservationsManager/Models/Entities/User.cs
@@ -2,5 +2,19 @@
 {
     public class User
     {
+        public Guid ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string EGN { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime HireDate { get; set; }
+        public bool Active { get; set; }
+        public DateTime FireDate { get; set; }
+        public bool Admin { get; set; }
+
     }
 }
