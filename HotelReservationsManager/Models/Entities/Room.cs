82c047ef87747db95f1be6ac09a73169af59f0ac
Kamui <Kamuicho>
2024-04-03 21:39:33 +0300

    Wrote Room class.

--- HotelReservationsManager/Models/Entities/Room.cs
+++ HotelReservationsManager/Models/Entities/Room.cs
@@ -1,6 +1,22 @@
 ﻿namespace HotelReservationsManager.Models.Entities
 {
+    public enum Type
+    {
+        Double,
+        TwoSingles,
+        Apartment,
+        Penthouse,
+        Maisonette
+    }
     public class Room
     {
+        public Guid ID { get; set; }
+        public int Capacity { get; set; }
+        public Type Type { get; set; }
+        public bool Available { get; set; }
+        public decimal PricePerAdultBed { get; set; }
+        public decimal PricePerChildBed { get; set; }
+        public string RoomNumber { get; set; }
     }
+
 }
