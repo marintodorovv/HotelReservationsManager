d4ac044ff56be97bd14e904c4b8a42a952a3b37d
Kamui <Kamuicho>
2024-04-03 21:39:19 +0300

    Wrote Client class.

--- HotelReservationsManager/Models/Entities/Client.cs
+++ HotelReservationsManager/Models/Entities/Client.cs
@@ -2,5 +2,12 @@
 {
     public class Client
     {
+        public Guid ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public bool Adult { get; set; }
+
     }
 }
