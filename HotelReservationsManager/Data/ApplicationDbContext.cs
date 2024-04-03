c2f83a7e206bbb71778e0df42c0a688b93f21dc7
Kamui <Kamuicho>
2024-04-03 21:40:10 +0300

    Wrote DbContext.

--- HotelReservationsManager/Data/ApplicationDbContext.cs
+++ HotelReservationsManager/Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using HotelReservationsManager.Models.Entities;
 
 namespace HotelReservationsManager.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        DbSet<Client> Clients { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<Room> Rooms { get; set; }
+        DbSet<Reservation> Reservations { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
